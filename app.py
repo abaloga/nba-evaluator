1f9bee633708726c2d1497512391378e443368fb
Anthony <baloga2001@gmail.com>
2025-07-13 14:13:23 -0400

    modified:   app.py 	modified:   nba_player_cache.json

--- app.py
+++ app.py
@@ -493,6 +493,7 @@ def get_shot_chart_data(player_id, season="2023-24"):
             player_id=player_id,
             team_id=0,
             season_nullable=season,
+            context_measure_simple='FGA',
             season_type_all_star='Regular Season'
         )
         
